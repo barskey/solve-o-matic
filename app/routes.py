e6be528127379ff232aca00bf6d7a02eb8d86342
Steve Barskey <barskey@gmail.com>
2019-02-11 19:59:14 -0800

    Updates to templats.

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,11 @@ from app import app
 @app.route('/index')
 def index():
     return render_template('index.html', title='Main Menu')
+	
+@app.route('/scan')
+def scan():
+	return render_template('scan.html', title='Scan')
+
+@app.route('/settings')
+def settings():
+	return render_template('settings.html', title='Settings')
