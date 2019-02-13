537c6170a62af3d29656893cb44ffb9292e89164
barskey <barskey@gmail.com>
2019-02-12 23:41:06 -0800

    Added fontawesome. Got camera started working. Updates to settings.

--- app/static/js/scan.js
+++ app/static/js/scan.js
@@ -1,32 +1,37 @@
 $( document ).ready( function() {
-    const $captureVideoButton = $( '#capture-button' );
-    const $screenshotButton = $( '#screenshot-button' );
-    const $img = $( '#screenshot img' );
-    const $video = $( '#screenshot video' );
+    const screenshotButton = $( '#screenshot-button' );
+    const img = $( '#screenshot img' );
+    const video = $( '#video' )[0]; // gets the DOM object from the jquery object
 
     const constraints = {
-        video: {width: {exact: 240}, height: {exact: 120}}
+        video: {width: {exact: 160}, height: {exact: 160}}
     };
 
     const canvas = document.createElement( 'canvas' );
 
-    console.log("Capturing video...");
-    navigator.mediaDevices.getUserMedia( constraints ).
-    then( handleSuccess ).catch( handleError );
+    navigator.mediaDevices.getUserMedia( constraints ).then(
+        function(stream) {
+            console.log("Capturing video...");
+            video.srcObject = stream;
+        }, function(error) {
+            console.error('Error: ', error);
+        }
+    );
 
     // TODO - convert button click to ajax call to take video screenshot
-    $screenshotButton.click( function() {
+    screenshotButton.click( function() {
         console.log("Screenshot btn clicked.");
-        canvas.width = $video.videoWidth;
-        canvas.height = $video.videoHeight;
-        canvas.getContext( '2d' ).drawImage( $video, 0, 0 );
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        canvas.getContext( '2d' ).drawImage( video, 0, 0 );
         // Other browsers will fall back to image/png
-        $img.src = canvas.toDataURL( 'image/webp' );
+        img.src = canvas.toDataURL( 'image/webp' );
     });
 
     function handleSuccess( stream ) {
-        $screenshotButton.disabled = false;
-        $video.srcObject = stream;
+        console.log("Capturing video...");
+        console.log(video);
+        video.srcObject = stream;
     }
 
     function handleError( error ) {
