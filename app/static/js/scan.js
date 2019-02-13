5685253dfd66cfc66902c0b3b1c75478c4920812
ZONE1\SBarskey <steve.barskey@flir.com>
2019-02-12 17:08:53 -0800

    Working on settings.

--- app/static/js/scan.js
+++ app/static/js/scan.js
@@ -1,28 +1,35 @@
 $( document ).ready( function() {
-    const captureVideoButton = $( '#capture-button' );
-    const screenshotButton = $( '#screenshot-button' );
-    const img = $('#screenshot-img');
-    const video = $('#screenshot-video');
+    const $captureVideoButton = $( '#capture-button' );
+    const $screenshotButton = $( '#screenshot-button' );
+    const $img = $( '#screenshot img' );
+    const $video = $( '#screenshot video' );
 
-    const canvas = document.createElement('canvas');
-
-    captureVideoButton.onclick = function() {
-        console.log("Capture btn clicked.");
-        navigator.mediaDevices.getUserMedia(constraints).
-        then(handleSuccess).catch(handleError);
+    const constraints = {
+        video: {width: {exact: 240}, height: {exact: 120}}
     };
 
-    screenshotButton.onclick = video.onclick = function() {
+    const canvas = document.createElement( 'canvas' );
+
+    console.log("Capturing video...");
+    navigator.mediaDevices.getUserMedia( constraints ).
+    then( handleSuccess ).catch( handleError );
+
+    // TODO - convert button click to ajax call to take video screenshot
+    $screenshotButton.click( function() {
         console.log("Screenshot btn clicked.");
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
-        canvas.getContext('2d').drawImage(video, 0, 0);
+        canvas.width = $video.videoWidth;
+        canvas.height = $video.videoHeight;
+        canvas.getContext( '2d' ).drawImage( $video, 0, 0 );
         // Other browsers will fall back to image/png
-        img.src = canvas.toDataURL('image/webp');
-    };
+        $img.src = canvas.toDataURL( 'image/webp' );
+    });
 
-    function handleSuccess(stream) {
-        screenshotButton.disabled = false;
-        video.srcObject = stream;
+    function handleSuccess( stream ) {
+        $screenshotButton.disabled = false;
+        $video.srcObject = stream;
+    }
+
+    function handleError( error ) {
+        console.error('Error: ', error);
     }
 });
