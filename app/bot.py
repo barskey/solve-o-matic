c88fe5b6665990f9a63524b1e861bbf9f71e8d45
Steve Barskey <>
2019-08-16 21:07:23 -0700

    trying pause

--- app/bot.py
+++ app/bot.py
@@ -57,8 +57,10 @@ class Bot(object):
             #self.kit.servo[self.twist_channel[grip]].angle = self.TWIST_POS[grip][1]
             t = threading.Thread(target=set_servo_angle, args=(self.grip_channel[grip],self.GRIP_POS[grip]['o'],))
             t.start()
+            time.sleep(0.5)
             u = threading.Thread(target=set_servo_angle, args=(self.twist_channel[grip],self.TWIST_POS[grip][1],))
             u.start()
+            time.sleep(0.5)
 
     def update_cal(self, cal_data):
         self.GRIP_POS['A'] = {
