9673cc36d3809eae6ad6917a5fb39819298cdf10
Steve Barskey <>
2019-08-31 20:43:09 -0700

    testing camera settings

--- app/bot.py
+++ app/bot.py
@@ -67,8 +67,11 @@ class Bot(object):
         self.camera.resolution = (160, 160)
         self.camera.iso = 100
         time.sleep(1)
-        self.camera.awb_mode = 'off'
         self.camera.shutter_speed = self.camera.exposure_speed
+        self.camera.exposure_mode = 'off'
+        g = self.camera.awb_gains
+        self.camera.awb_mode = 'off'
+        self.camera.awb_gains = g
 
     
     def init_servos(self):
