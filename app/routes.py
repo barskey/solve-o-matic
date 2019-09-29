73862e765f50b889aee7fe0d249776a682981d16
barskey <barskey@gmail.com>
2019-09-29 15:10:50 -0700

    jsonofy all returns

--- app/routes.py
+++ app/routes.py
@@ -28,7 +28,7 @@ def update_solve():
 	solve_to = request.form['solve_to']
 	moves = mybot.cube.update_solve_to(solve_to)
 	solve_img = PATTERNS[solve_to][0]
-	return {'solve_to': solve_to, 'moves': moves, 'solve_img': solve_img}
+	return jsonofy({'solve_to': solve_to, 'moves': moves, 'solve_img': solve_img})
 
 @app.route('/go_solve')
 def go_solve():
@@ -58,7 +58,7 @@ def set_calibrate():
 	if setting in ['min', 'max']:
 		mybot.init_servos()
 	
-	return {'prop': prop, 'setting': setting, 'value': new_value}
+	return jsonify({'prop': prop, 'setting': setting, 'value': new_value})
 
 @app.route('/set_color_slider', methods=['POST'])
 def set_color_slider():
@@ -69,15 +69,15 @@ def set_color_slider():
 	cal.set_property('color_limits', setting, float(value))
 	mybot.update_cal(cal)
 	
-	return mybot.process_face()
+	return jsonify(mybot.process_face())
 
 @app.route('/get_sites', methods=['POST'])
 def get_sites():
-	return {'sites': cal.sites}
+	return jsonify({'sites': cal.sites})
 
 @app.route('/get_face_colors', methods=['POST'])
 def get_face_colors():
-	return mybot.process_face()
+	return jsonify(mybot.process_face())
 
 @app.route('/move_gripper', methods=['POST'])
 def move_gripper():
@@ -92,13 +92,13 @@ def move_gripper():
 	elif cmd in ['min', 'max']:
 		result = mybot.twist(gripper, cmd)
 	
-	return {'code': result[0], 'msg': result[1]}
+	return jsonify({'code': result[0], 'msg': result[1]})
 
 @app.route('/scan_next', methods=['POST'])
 def scan_next():
 	if request.form['start'] == 'true':
 		result = mybot.start_scan()
-		return {'msg': result[1], 'result': result[0]}
+		return jsonofy({'msg': result[1], 'result': result[0]})
 	else:
 		r = mybot.scan_move()
 		if r == 1:
@@ -107,4 +107,4 @@ def scan_next():
 		else:
 			result = mybot.process_face()
 			result['result'] = 0
-			return result
+			return jsonofy(result)
